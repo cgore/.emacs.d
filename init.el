b86f968fa98463f23b959ce00180d7a8655c37b2
Christopher Mark Gore <cgore@cgore.com>
2013-03-05 20:01:23 +0000

    Adding conditional GUI code.

--- init.el
+++ init.el
@@ -64,23 +64,24 @@
  '(size-indication-mode t)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
 
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil
-		:stipple nil
-		:inverse-video nil
-		:box nil
-		:strike-through nil
-		:overline nil
-		:underline nil
-		:slant normal
-		:weight normal
-		:height 98
-		:width normal
-		:background "white"
-		:foreground "black"
-		:foundry "bitstream"
-		:family "Bitstream Vera Sans Mono")))))
+(if window-system ; Only if we are in a GUI.
+    (custom-set-faces
+     ;; custom-set-faces was added by Custom.
+     ;; If you edit it by hand, you could mess it up, so be careful.
+     ;; Your init file should contain only one such instance.
+     ;; If there is more than one, they won't work right.
+     '(default ((t (:inherit nil
+			     :stipple nil
+			     :inverse-video nil
+			     :box nil
+			     :strike-through nil
+			     :overline nil
+			     :underline nil
+			     :slant normal
+			     :weight normal
+			     :height 98
+			     :width normal
+			     :background "white"
+			     :foreground "black"
+			     :foundry "bitstream"
+			     :family "Bitstream Vera Sans Mono"))))))
