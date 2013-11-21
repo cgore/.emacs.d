bac5e2b72649c237da9afaf2d812e355a3b52845
Christopher Mark Gore <cgore@cgore.com>
2013-11-21 11:32:41 -0600

    Some more symbols.

--- init.el
+++ init.el
@@ -357,17 +357,26 @@
 (defun tex-input ()
   (interactive)
   (set-input-method "TeX"))
+
 (global-set-key (kbd "<f9> f") 'french-input)
 (global-set-key (kbd "<f9> g") 'greek-input)
 (global-set-key (kbd "<f9> t") 'tex-input)
 (global-set-key (kbd "<f9> u") 'ucs-input)
+
+(global-set-key (kbd "<f9> c") "¢")
+(global-set-key (kbd "<f9> B") "฿")
+(global-set-key (kbd "<f9> E") "€")
+(global-set-key (kbd "<f9> L") "£")
+
+(global-set-key (kbd "<f9> C") "©")
+(global-set-key (kbd "<f9> R") "®")
+(global-set-key (kbd "<f9> TM") "™")
 
 (global-set-key (kbd "<f9> ->") "→")
 (global-set-key (kbd "<f9> <-") "←")
 (global-set-key (kbd "<f9> =>") "⇒")
 (global-set-key (kbd "<f9> <=") "⇐")
 (global-set-key (kbd "<f9> inf") "∞")
-
 (global-set-key (kbd "<f9> :)") "☻")
 
 
