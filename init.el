be5d5b2839a70d9f35b637fe60e94dfa29d9e78e
Christopher Mark Gore <cgore@cgore.com>
2013-11-21 13:43:21 -0600

    Cookies in w3m.

--- init.el
+++ init.el
@@ -319,7 +319,8 @@
       w3m-file-name-coding-system 'utf-8
       w3m-input-coding-system 'utf-8
       w3m-output-coding-system 'utf-8
-      w3m-terminal-coding-system 'utf-8)
+      w3m-terminal-coding-system 'utf-8
+      w3m-use-cookies t)
 (defun browse-url-at-point-firefox ()
   (interactive)
   (let ((browse-url-browser-function 'browse-url-firefox))
