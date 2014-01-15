6736b6e46c452dedcf2a63faf9d766952719be1e
Christopher Mark Gore <cgore@cgore.com>
2014-01-15 11:49:53 -0600

    Adding some eshell commands.

--- init.el
+++ init.el
@@ -457,6 +457,34 @@
           python-mode-hook
           ruby-mode-hook))
 
+;;; Eshell Functions
+
+;; Taken from <http://www.emacswiki.org/emacs/EshellFunctions>
+(defun eshell/emacs (&rest args)
+  "Open a file in emacs. Some habits die hard."
+  (if (null args)
+      ;; If I just ran "emacs", I probably expect to be launching
+      ;; Emacs, which is rather silly since I'm already in Emacs.
+      ;; So just pretend to do what I ask.
+      (bury-buffer)
+    ;; We have to expand the file names or else naming a directory in an
+    ;; argument causes later arguments to be looked for in that directory,
+    ;; not the starting directory
+    (mapc #'find-file (mapcar #'expand-file-name (eshell-flatten-list (reverse args))))))
+
+(defun format-commands (&rest commands)
+  (mapcar (lambda (command)
+            (shell-command (apply 'format command)))
+          commands))
+
+(defun eshell/git-branch-here (branch)
+  (format-commands `("git push origin :%s" ,branch)
+                   `("git branch -d %s" ,branch)
+                   `("git checkout -b %s" ,branch)
+                   `("git push origin %s:%s" ,branch ,branch)))
+
+(defun eshell/git-experimental-here ()
+  (eshell/git-branch-here "experimental"))
 
 ;;; Atlassian stuff
 (when (abaddon?)
