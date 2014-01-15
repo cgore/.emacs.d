3328e192aad3fed19bd20390d6d592c7d1c7e326
Christopher Mark Gore <cgore@cgore.com>
2014-01-14 20:19:16 -0600

    Display the date in the mode line.

--- init.el
+++ init.el
@@ -117,6 +117,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(display-time-mode t)
+ '(display-time-day-and-date t)
  '(inhibit-startup-screen t)
  '(tool-bar-mode nil)
  '(menu-bar-mode nil)
