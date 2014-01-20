ccc53de4ceb25899d6bb0ec6c18515824691693b
Christopher Mark Gore <cgore@cgore.com>
2014-01-20 10:27:50 -0600

    Adding Maxima stuff.

--- init.el
+++ init.el
@@ -510,3 +510,12 @@
           org-mode-hook
           python-mode-hook
           ruby-mode-hook))
+
+;;; Maxima
+(add-to-list 'load-path "/usr/share/maxima/5.32.1/emacs/")
+(autoload 'maxima-mode "maxima" "Maxima mode" t)
+(autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
+(autoload 'maxima "maxima" "Maxima interaction" t)
+(autoload 'imath-mode "imath" "Imath mode for math formula input" t)
+(setq imaxima-use-maxima-mode-flag t)
+(add-to-list 'auto-mode-alist '("\\.ma[cx]" . maxima-mode))
