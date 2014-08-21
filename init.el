9e82ea4bfd779f9878624c4deb454e3b1faeb51c
Christopher Mark Gore <cgore@cgore.com>
2014-08-21 12:32:53 -0500

    Deleting duplicate font setup.

--- init.el
+++ init.el
@@ -632,13 +632,6 @@
 (require 'ace-jump-mode)
 (global-set-key (kbd "C-'") 'ace-jump-mode)
 (global-set-key (kbd "C-M-'") 'ace-jump-mode-pop-mark)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Monaco")))))
-
 
 ;;; Multiple Cursors
 (require 'multiple-cursors)
