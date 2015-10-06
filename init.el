c0330a4f2f42a40492083f82aff3c541ae266479
Christopher Mark Gore <chris.gore@climate.com>
2015-10-06 11:07:44 -0500

    Adding shortcut keys for magit tree listings.

--- init.el
+++ init.el
@@ -475,6 +475,8 @@
 ;;; Git
 (add-to-list 'auto-mode-alist '("\\.gitconfig" . conf-mode))
 (global-set-key (kbd "<f2> g") 'magit-status)
+(global-set-key (kbd "<f2> l") 'magit-log-current)
+(global-set-key (kbd "<f2> t") 'magit-log-all)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 ;;; Buffer navigation.
