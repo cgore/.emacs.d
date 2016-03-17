d00e4e5dc59156ddfeb3c9b2db488da871166c2c
Christopher Mark Gore <chris.gore@climate.com>
2016-03-17 14:22:56 -0500

    Spacing.

--- init.el
+++ init.el
@@ -443,7 +443,9 @@
 
 ;;; Web stuff.
 (setq browse-url-browser-function 'w3m-browse-url)
+
 (global-set-key (kbd "<f6> w") 'browse-url-at-point)
+
 (setq w3m-coding-system 'utf-8
       w3m-file-coding-system 'utf-8
       w3m-file-name-coding-system 'utf-8
@@ -451,6 +453,7 @@
       w3m-output-coding-system 'utf-8
       w3m-terminal-coding-system 'utf-8
       w3m-use-cookies t)
+
 (defun browse-url-at-point-firefox ()
   (interactive)
   (let ((browse-url-browser-function 'browse-url-firefox))
