0975467c88bf9b6c0ca3f242f29c4d0a0f8ac371
Christopher Mark Gore <chris.gore@climate.com>
2016-09-23 14:14:26 -0500

    Breaking out the eshell stuff to it's own file.

---
+++ eshell.el
@@ -0,0 +1,65 @@
+;;;; Copyright © 2013-2016, Christopher Mark Gore,
+;;;; Soli Deo Gloria,
+;;;; All rights reserved.
+;;;;
+;;;; 2317 South River Road, Saint Charles, Missouri 63303 USA.
+;;;; Web: http://cgore.com
+;;;; Email: cgore@cgore.com
+;;;;
+;;;; Redistribution and use in source and binary forms, with or without
+;;;; modification, are permitted provided that the following conditions are met:
+;;;;
+;;;;     * Redistributions of source code must retain the above copyright
+;;;;       notice, this list of conditions and the following disclaimer.
+;;;;
+;;;;     * Redistributions in binary form must reproduce the above copyright
+;;;;       notice, this list of conditions and the following disclaimer in the
+;;;;       documentation and/or other materials provided with the distribution.
+;;;;
+;;;;     * Neither the name of Christopher Mark Gore nor the names of other
+;;;;       contributors may be used to endorse or promote products derived from
+;;;;       this software without specific prior written permission.
+;;;;
+;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;;; POSSIBILITY OF SUCH DAMAGE.
+
+(require 'eshell)
+(setq eshell-path-env (concat "/usr/local/bin" ":"
+                              eshell-path-env))
+
+
+;; Taken from <http://www.emacswiki.org/emacs/EshellFunctions>
+(defun eshell/emacs (&rest args)
+  "Open a file in emacs. Some habits die hard."
+  (if (null args)
+      ;; If I just ran "emacs", I probably expect to be launching
+      ;; Emacs, which is rather silly since I'm already in Emacs.
+      ;; So just pretend to do what I ask.
+      (bury-buffer)
+    ;; We have to expand the file names or else naming a directory in an
+    ;; argument causes later arguments to be looked for in that directory,
+    ;; not the starting directory
+    (mapc #'find-file (mapcar #'expand-file-name (eshell-flatten-list (reverse args))))))
+
+(defun format-commands (&rest commands)
+  (mapcar (lambda (command)
+            (shell-command (apply 'format command)))
+          commands))
+
+(defun eshell/git-branch-here (branch)
+  (format-commands `("git push origin :%s" ,branch)
+                   `("git branch -d %s" ,branch)
+                   `("git checkout -b %s" ,branch)
+                   `("git push origin %s:%s" ,branch ,branch)))
+
+(defun eshell/git-experimental-here ()
+  (eshell/git-branch-here "experimental"))
--- init.el
+++ init.el
@@ -591,39 +591,7 @@
           python-mode-hook
           ruby-mode-hook))
 
-(require 'eshell)
-(setq eshell-path-env (concat "/usr/local/bin" ":"
-                              eshell-path-env))
-
-;;; Eshell Functions
-
-;; Taken from <http://www.emacswiki.org/emacs/EshellFunctions>
-(defun eshell/emacs (&rest args)
-  "Open a file in emacs. Some habits die hard."
-  (if (null args)
-      ;; If I just ran "emacs", I probably expect to be launching
-      ;; Emacs, which is rather silly since I'm already in Emacs.
-      ;; So just pretend to do what I ask.
-      (bury-buffer)
-    ;; We have to expand the file names or else naming a directory in an
-    ;; argument causes later arguments to be looked for in that directory,
-    ;; not the starting directory
-    (mapc #'find-file (mapcar #'expand-file-name (eshell-flatten-list (reverse args))))))
-
-(defun format-commands (&rest commands)
-  (mapcar (lambda (command)
-            (shell-command (apply 'format command)))
-          commands))
-
-(defun eshell/git-branch-here (branch)
-  (format-commands `("git push origin :%s" ,branch)
-                   `("git branch -d %s" ,branch)
-                   `("git checkout -b %s" ,branch)
-                   `("git push origin %s:%s" ,branch ,branch)))
-
-(defun eshell/git-experimental-here ()
-  (eshell/git-branch-here "experimental"))
-
+(load "~/.emacs.d/eshell.el")
 
 ;;; Line Numbers
 (unless window-system ; Only when in non-GUI
@@ -681,7 +649,6 @@
       imaxima-linearize-flag nil)
 (add-to-list 'auto-mode-alist '("\\.ma[cx]" . maxima-mode))
 (global-set-key (kbd "<XF86Calculator>") 'maxima)
-
 
 
 ;;; Markdown
