45313e82f0d384a84057007bebfcaa87a07c989f
Christopher Mark Gore <chris.gore@climate.com>
2017-05-25 12:46:03 -0500

    Moving the ERC setup to it's own file.

---
+++ erc-setup.el
@@ -0,0 +1,69 @@
+;;;; Copyright © 2013-2017, Christopher Mark Gore,
+;;;; Soli Deo Gloria,
+;;;; All rights reserved.
+;;;;
+;;;; 2317 South River Road, Saint Charles, Missouri 63303 USA.
+;;;; Web: http://cgore.com
+;;;; Email: cgore@cgore.com
+;;;;
+;;;; Redistribution and use in source and binary forms, with or without
+;;;; modification, are permitted provided that the following conditions are met:
+;;;;
+;;;;     * Redistributions of source code must retain the above copyright
+;;;;       notice, this list of conditions and the following disclaimer.
+;;;;
+;;;;     * Redistributions in binary form must reproduce the above copyright
+;;;;       notice, this list of conditions and the following disclaimer in the
+;;;;       documentation and/or other materials provided with the distribution.
+;;;;
+;;;;     * Neither the name of Christopher Mark Gore nor the names of other
+;;;;       contributors may be used to endorse or promote products derived from
+;;;;       this software without specific prior written permission.
+;;;;
+;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;;; POSSIBILITY OF SUCH DAMAGE.
+
+;;; ERC: Emacs IRC
+(setq erc-log-channels-directory "~/.emacs.d/erc/log/"
+      erc-nick "cgore"
+      erc-user-full-name "Christopher Mark Gore"
+      erc-email-userid "cgore@cgore.com"
+      erc-interpret-mirc-color t
+      erc-kill-buffer-on-part t
+      erc-kill-queries-on-quit t
+      erc-kill-server-buffer-on-quit t
+      erc-save-buffer-on-part t
+      erc-save-queries-on-quit t
+      erc-log-write-after-send t
+      erc-log-write-after-insert t
+      erc-autojoin-channels-alist '(("freenode.net"
+                                     "#clojure" "#jesus" "#lisp")))
+
+(setq erc-prompt
+      (lambda ()
+        (erc-propertize (if (and (boundp 'erc-default-recipients)
+                                 (erc-default-target))
+                          (concat (erc-default-target) ">")
+                          (concat "ERC>"))
+                        'read-only t 'rear-nonsticky t 'front-nonsticky t)))
+(require 'erc-services)
+(erc-services-mode 1)
+(setq erc-prompt-for-nickserv-password nil)
+(add-to-list 'erc-modules 'log)
+(erc-update-modules)
+
+;; Set your erc-nickserv-passwords in this file.  Example:
+;;(setq erc-nickserv-passwords
+;;      `((freenode (("whoYouAre" . "yourSecretPassword")))))
+(setq ercpass-el "~/.emacs.d/ercpass.el")
+(when (file-exists-p ercpass-el)
+  (load "~/.emacs.d/ercpass.el"))
--- init.el
+++ init.el
@@ -251,41 +251,7 @@
 
 (require 'tls)
 
-;;; ERC: Emacs IRC
-(setq erc-log-channels-directory "~/.emacs.d/erc/log/"
-      erc-nick "cgore"
-      erc-user-full-name "Christopher Mark Gore"
-      erc-email-userid "cgore@cgore.com"
-      erc-interpret-mirc-color t
-      erc-kill-buffer-on-part t
-      erc-kill-queries-on-quit t
-      erc-kill-server-buffer-on-quit t
-      erc-save-buffer-on-part t
-      erc-save-queries-on-quit t
-      erc-log-write-after-send t
-      erc-log-write-after-insert t
-      erc-autojoin-channels-alist '(("freenode.net"
-                                     "#clojure" "#jesus" "#lisp")))
-
-(setq erc-prompt
-      (lambda ()
-        (erc-propertize (if (and (boundp 'erc-default-recipients)
-                                 (erc-default-target))
-                          (concat (erc-default-target) ">")
-                          (concat "ERC>"))
-                        'read-only t 'rear-nonsticky t 'front-nonsticky t)))
-(require 'erc-services)
-(erc-services-mode 1)
-(setq erc-prompt-for-nickserv-password nil)
-(add-to-list 'erc-modules 'log)
-(erc-update-modules)
-
-;; Set your erc-nickserv-passwords in this file.  Example:
-;;(setq erc-nickserv-passwords
-;;      `((freenode (("whoYouAre" . "yourSecretPassword")))))
-(setq ercpass-el "~/.emacs.d/ercpass.el")
-(when (file-exists-p ercpass-el)
-  (load "~/.emacs.d/ercpass.el"))
+(load "~/.emacs.d/erc-setup.el")
 
 ;;; Org Mode
 (require 'org)
