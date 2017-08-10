6fe40038f61f61dd404b8e97b81e90939e4829c2
Christopher Mark Gore <chris.gore@climate.com>
2017-08-10 15:59:14 -0500

    better neotree shortcuts

--- init.el
+++ init.el
@@ -419,7 +419,9 @@
 
 ;; Neotree - a nice directory listing panel on the left side of your Emacs.
 (require 'neotree)
-(global-set-key (kbd "<f8>") 'neotree-toggle)
+(global-set-key (kbd "<f8> <f8>") 'neotree-toggle)
+(global-set-key (kbd "<f8> /") 'neotree-dir)
+(global-set-key (kbd "<f8> .") 'neotree-find)
 
 (require 'echo-bell)
 (echo-bell-mode)
