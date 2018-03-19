24fc0f91dbf5f6f56114430ff001309037e606d2
Christopher Mark Gore <cgore@cgore.com>
2018-03-19 09:54:38 -0500

    cool little utility function to round a number

--- utilities.el
+++ utilities.el
@@ -43,3 +43,20 @@
 ;;; color these functions like keywords
 (font-lock-add-keywords 'emacs-lisp-mode
                         '(("(\\(icurry\\)[ \t\n\r]" 1 font-lock-keyword-face)))
+
+;; Rounding numbers
+(defun get-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0123456789.-")
+  (or (looking-at "[0123456789.-]+")
+      (error "No number at point"))
+  (string-to-number (match-string 0)))
+
+(defun round-number-at-point-to-decimals (decimal-count)
+  (interactive "NDecimal count: ")
+  (let ((mult (expt 10 decimal-count)))
+    (replace-match (number-to-string
+                    (/ (fround
+                        (* mult
+                           (get-number-at-point)))
+                       mult)))))
