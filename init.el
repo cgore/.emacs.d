a10e7414df361683d6b36f013479c817256f5bba
Christopher Mark Gore <cgore@cgore.com>
2018-05-24 11:52:17 -0500

    breaking out the web stuff to it's own file

--- init.el
+++ init.el
@@ -234,27 +234,7 @@
 (setq org-log-done t)
 (add-hook 'org-mode-hook 'org-indent-mode)
 
-;;; Web stuff.
-(setq browse-url-browser-function 'w3m-browse-url)
-
-(global-set-key (kbd "<f6> w") 'browse-url-at-point)
-
-(setq w3m-coding-system 'utf-8
-      w3m-file-coding-system 'utf-8
-      w3m-file-name-coding-system 'utf-8
-      w3m-input-coding-system 'utf-8
-      w3m-output-coding-system 'utf-8
-      w3m-terminal-coding-system 'utf-8
-      w3m-use-cookies t)
-
-(defun browse-url-at-point-firefox ()
-  (interactive)
-  (let ((browse-url-browser-function 'browse-url-firefox))
-    (browse-url-at-point)))
-(global-set-key (kbd "<f6> f") 'browse-url-at-point-firefox)
-(global-set-key (kbd "<f6> g")
-                (icurry 'browse-url-firefox "http://www.google.com"))
-
+(load "~/.emacs.d/www.el")
 (load "~/.emacs.d/thinking-bicycle.el")
 
 ;;; TeX and LaTeX
---
+++ www.el
@@ -0,0 +1,53 @@
+;;;; Copyright © 2013-2018, Christopher Mark Gore,
+;;;; Soli Deo Gloria,
+;;;; All rights reserved.
+;;;;
+;;;; 2317 South River Road, Saint Charles, Missouri 63303 USA.
+;;;; Web: http://cgore.com
+;;;; Email: cgore@cgore.com
+;;;;
+;;;; Redistribution and use in source and binary forms, with or without
+;;;; modification, are permitted provided that the following conditions are met:
+;;;;
+;;;;     * Redistributions of source code must retain the above copyright
+;;;;       notice, this list of conditions and the following disclaimer.
+;;;;
+;;;;     * Redistributions in binary form must reproduce the above copyright
+;;;;       notice, this list of conditions and the following disclaimer in the
+;;;;       documentation and/or other materials provided with the distribution.
+;;;;
+;;;;     * Neither the name of Christopher Mark Gore nor the names of other
+;;;;       contributors may be used to endorse or promote products derived from
+;;;;       this software without specific prior written permission.
+;;;;
+;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;;; POSSIBILITY OF SUCH DAMAGE.
+
+(setq browse-url-browser-function 'w3m-browse-url)
+
+(global-set-key (kbd "<f6> w") 'browse-url-at-point)
+
+(setq w3m-coding-system 'utf-8
+      w3m-file-coding-system 'utf-8
+      w3m-file-name-coding-system 'utf-8
+      w3m-input-coding-system 'utf-8
+      w3m-output-coding-system 'utf-8
+      w3m-terminal-coding-system 'utf-8
+      w3m-use-cookies t)
+
+(defun browse-url-at-point-firefox ()
+  (interactive)
+  (let ((browse-url-browser-function 'browse-url-firefox))
+    (browse-url-at-point)))
+(global-set-key (kbd "<f6> f") 'browse-url-at-point-firefox)
+(global-set-key (kbd "<f6> g")
+                (icurry 'browse-url-firefox "http://www.google.com"))
