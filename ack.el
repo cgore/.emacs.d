c7ddbc18568a7f46c6e2a6ba7255dd25758f7783
Christopher Mark Gore <cgore@cgore.com>
2018-05-24 11:52:32 -0500

    Sticking ack on a key

--- ack.el
+++ ack.el
@@ -38,3 +38,4 @@
 (defalias 'ack-same 'ack-and-a-half-same)
 (defalias 'ack-find-file 'ack-and-a-half-find-file)
 (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+(global-set-key (kbd "<f8> a") 'ack)
