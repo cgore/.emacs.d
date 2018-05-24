5f5b5e3b27800c41bd04998f72db10a78b955411
Christopher Mark Gore <cgore@cgore.com>
2018-05-24 11:03:41 -0500

    breaking out the foreign glyphs into it's own file

--- init.el
+++ init.el
@@ -270,42 +270,7 @@
 (global-set-key (kbd "<XF86Back>") 'previous-buffer)
 (global-set-key (kbd "<XF86Forward>") 'next-buffer)
 
-
-;;; Odd letters and glyphs.
-(defun french-input ()
-  (interactive)
-  (set-input-method "french-postfix"))
-(defun greek-input ()
-  (interactive)
-  (set-input-method "greek"))
-(defun ucs-input ()
-  (interactive)
-  (set-input-method "ucs"))
-(defun tex-input ()
-  (interactive)
-  (set-input-method "TeX"))
-
-(global-set-key (kbd "<f9> f") 'french-input)
-(global-set-key (kbd "<f9> g") 'greek-input)
-(global-set-key (kbd "<f9> t") 'tex-input)
-(global-set-key (kbd "<f9> u") 'ucs-input)
-
-(global-set-key (kbd "<f9> c") "¢")
-(global-set-key (kbd "<f9> B") "฿")
-(global-set-key (kbd "<f9> E") "€")
-(global-set-key (kbd "<f9> L") "£")
-
-(global-set-key (kbd "<f9> C") "©")
-(global-set-key (kbd "<f9> R") "®")
-(global-set-key (kbd "<f9> TM") "™")
-
-(global-set-key (kbd "<f9> ->") "→")
-(global-set-key (kbd "<f9> <-") "←")
-(global-set-key (kbd "<f9> =>") "⇒")
-(global-set-key (kbd "<f9> <=") "⇐")
-(global-set-key (kbd "<f9> inf") "∞")
-(global-set-key (kbd "<f9> :)") "☻")
-
+(load "~/.emacs.d/noforn.el")
 
 ;;; Buffers
 (global-set-key (kbd "<f2> r") 'rename-buffer)
@@ -315,7 +280,6 @@
 
 ;;; Rectangles
 (global-set-key (kbd "C-x r I") 'string-insert-rectangle)
-
 
 ;;; GPG stuff.
 (require 'epa-file)
