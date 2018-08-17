e6d4e32043856006dea749288c4b5b07773985b4
Christopher Mark Gore <cgore@cgore.com>
2018-08-17 17:00:44 -0500

    License updated, and moved into a single file

--- appearance.el
+++ appearance.el
@@ -1,37 +1,3 @@
-;;;; Copyright © 2013-2018, Christopher Mark Gore,
-;;;; Soli Deo Gloria,
-;;;; All rights reserved.
-;;;;
-;;;; 2317 South River Road, Saint Charles, Missouri 63303 USA.
-;;;; Web: http://cgore.com
-;;;; Email: cgore@cgore.com
-;;;;
-;;;; Redistribution and use in source and binary forms, with or without
-;;;; modification, are permitted provided that the following conditions are met:
-;;;;
-;;;;     * Redistributions of source code must retain the above copyright
-;;;;       notice, this list of conditions and the following disclaimer.
-;;;;
-;;;;     * Redistributions in binary form must reproduce the above copyright
-;;;;       notice, this list of conditions and the following disclaimer in the
-;;;;       documentation and/or other materials provided with the distribution.
-;;;;
-;;;;     * Neither the name of Christopher Mark Gore nor the names of other
-;;;;       contributors may be used to endorse or promote products derived from
-;;;;       this software without specific prior written permission.
-;;;;
-;;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-;;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-;;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-;;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-;;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-;;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-;;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-;;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-;;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-;;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-;;;; POSSIBILITY OF SUCH DAMAGE.
-
 (defun dark-background ()
   (interactive)
   (load-theme 'sanityinc-tomorrow-eighties t)
@@ -52,8 +18,6 @@
   (interactive)
   (when (window-system)
     (set-default-font "Fira Code"))
-  (when darwin?
-    (mac-auto-operator-composition-mode))
   (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
                  (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
                  (36 . ".\\(?:>\\)")
@@ -82,7 +46,7 @@
       (set-char-table-range composition-function-table (car char-regexp)
                             `([,(cdr char-regexp) 0 font-shape-gstring])))))
 
-;(set-fira-code-font)
+;;(set-fira-code-font)
 
 (defun default-font-and-theme ()
   (interactive)
