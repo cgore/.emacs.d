54480bf8db0019ba7a481749e176574582d4d9be
Christopher Mark Gore <cgore@cgore.com>
2018-09-04 10:13:06 -0500

    2-space indent for JavaScript

--- javascript.el
+++ javascript.el
@@ -2,13 +2,13 @@
 (when (or (linux?) (darwin?))
   (require 'json-mode))
 (require 'rjsx-mode)
-(setq js-indent-level 4 ; js-mode
-      javascript-indent-level 4 ; javascript-mode
-      web-mode-markup-indent-offset 4
-      web-mode-markup-indent-offset 4  ; web-mode, html tag in html file
-      web-mode-css-indent-offset 4 ; web-mode, css in html file
-      web-mode-code-indent-offset 4  ; web-mode, js code in html file
-      css-indent-level 4)  ; css-mode
+(setq js-indent-level 2 ; js-mode
+      javascript-indent-level 2 ; javascript-mode
+      web-mode-markup-indent-offset 2
+      web-mode-markup-indent-offset 2  ; web-mode, html tag in html file
+      web-mode-css-indent-offset 2 ; web-mode, css in html file
+      web-mode-code-indent-offset 2  ; web-mode, js code in html file
+      css-indent-level 2)  ; css-mode
 
 
 (setq web-mode-content-types-alist
