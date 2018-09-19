a7423a309a9a94a031191c125ea73673c1064979
Christopher Mark Gore <cgore@cgore.com>
2018-09-19 13:49:29 -0500

    Emacs for lots more places

--- git.el
+++ git.el
@@ -5,3 +5,8 @@
 (setq magit-last-seen-setup-instructions "1.4.0")
 (require 'magit-gitflow)
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+
+;; https://github.com/magit/with-editor
+(add-hook 'shell-mode-hook  'with-editor-export-editor)
+(add-hook 'term-exec-hook   'with-editor-export-editor)
+(add-hook 'eshell-mode-hook 'with-editor-export-editor)
