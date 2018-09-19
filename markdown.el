f73e2b3e137b30ea7a03aec356ea1f547ec97370
Christopher Mark Gore <cgore@cgore.com>
2018-09-19 13:49:15 -0500

    tweak to new-notes fn

--- markdown.el
+++ markdown.el
@@ -2,13 +2,13 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-(defun notes ()
+(defun new-notes ()
   (interactive)
   (let ((newbuf (generate-new-buffer
-                 (format-time-string "Untitled Notes - %a %-m/%d/%Y %-I:%M:%S %p "
+                 (format-time-string "Untitled Notes - %a %-m/%d/%Y %-I:%M:%S %p.md"
                                      (current-time)))))
     (switch-to-buffer newbuf)
     (setq buffer-offer-save t)
     (markdown-mode)
     newbuf))
-(global-set-key (kbd "<f8> n") 'notes)
+(global-set-key (kbd "<f8> n") 'new-notes)
