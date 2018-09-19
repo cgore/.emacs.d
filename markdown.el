20bcedc5ec3fba93c7a23af6d4b2082e3f28787a
Christopher Mark Gore <cgore@cgore.com>
2018-09-19 13:43:33 -0500

    Shortcut to quick markdown notes

--- markdown.el
+++ markdown.el
@@ -1,3 +1,14 @@
 (require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(defun notes ()
+  (interactive)
+  (let ((newbuf (generate-new-buffer
+                 (format-time-string "Untitled Notes - %a %-m/%d/%Y %-I:%M:%S %p "
+                                     (current-time)))))
+    (switch-to-buffer newbuf)
+    (setq buffer-offer-save t)
+    (markdown-mode)
+    newbuf))
+(global-set-key (kbd "<f8> n") 'notes)
