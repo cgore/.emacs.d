166fa256de181e46ea97bc21226e8a372d2c8d52
Christopher Mark Gore <cgore@cgore.com>
2018-11-13 16:17:26 -0600

    more awesome eshell/grcode

--- gr.el
+++ gr.el
@@ -16,7 +16,14 @@
     (cd ticket-directory)))
 
 (defun eshell/grcode (repo)
-  (cd (concat "/Users/cgore/gr/code/" repo)))
+  "Go to the directory for a Guaranteed Rate REPO."
+  (let* ((code-directory "/Users/cgore/gr/code/")
+         (repo-directory (concat code-directory repo)))
+    (when (not (file-exists-p repo-directory))
+      (eshell-printn (concat "Checking out a local copy of " repo " from github ..."))
+      (cd code-directory)
+      (shell-command (concat "git clone git@github.com:Guaranteed-Rate/" repo ".git")))
+    (cd repo-directory)))
 
 (defun eshell/field-report ()
   (eshell/grcode "field-report"))
