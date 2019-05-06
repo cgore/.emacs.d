f74ac6decd3ef68736510d89900fc23472efe802
Christopher Mark Gore <cgore@cgore.com>
2019-05-06 15:00:30 -0500

    Getting rid of magit-gitflow

--- git.el
+++ git.el
@@ -3,8 +3,6 @@
 (global-set-key (kbd "<f2> l") 'magit-log-current)
 (global-set-key (kbd "<f2> t") 'magit-log-all)
 (setq magit-last-seen-setup-instructions "1.4.0")
-(require 'magit-gitflow)
-(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 
 ;; https://github.com/magit/with-editor
 (add-hook 'shell-mode-hook  'with-editor-export-editor)
