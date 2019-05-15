cba1b87608bf3492dd65df21d2654a9365478bf8
Christopher Mark Gore <cgore@cgore.com>
2019-05-15 09:34:17 -0500

    I need this hideshow stuff to be in a hook after all

--- hideshow.el
+++ hideshow.el
@@ -1,10 +1,12 @@
 ;;;; <https://www.gnu.org/software/emacs/manual/html_node/emacs/Hideshow.html>
 
-(define-key hs-minor-mode-map (kbd "C-c h") 'hs-hide-block)
-(define-key hs-minor-mode-map (kbd "C-c s") 'hs-show-block)
-(define-key hs-minor-mode-map (kbd "C-c t") 'hs-toggle-hiding)
-(define-key hs-minor-mode-map (kbd "C-c H") 'hs-hide-all)
-(define-key hs-minor-mode-map (kbd "C-c S") 'hs-show-all)
+(add-hook 'hs-minor-mode-hook
+	  (lambda ()
+	    (define-key hs-minor-mode-map (kbd "C-c h") 'hs-hide-block)
+	    (define-key hs-minor-mode-map (kbd "C-c s") 'hs-show-block)
+	    (define-key hs-minor-mode-map (kbd "C-c t") 'hs-toggle-hiding)
+	    (define-key hs-minor-mode-map (kbd "C-c H") 'hs-hide-all)
+	    (define-key hs-minor-mode-map (kbd "C-c S") 'hs-show-all)))
 
 (add-hook 'clojure-mode-hook    'hs-minor-mode)
 (add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
