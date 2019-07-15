b2bafac32c8c0b0915391cff27247bf7a9efad3e
Christopher Mark Gore <cgore@cgore.com>
2019-07-15 16:20:38 -0500

    Get non-recursive SSM parameters

--- aws.el
+++ aws.el
@@ -1,4 +1,9 @@
-(defun ssm-decrypt (ssm-path)
+(defun ssm-decrypt (ssm-parameter-name)
+  (interactive "sSSM path? ")
+  (shell-command (format "aws ssm get-parameter --name %s --with-decryption"
+                         ssm-parameter-name)))
+
+(defun ssm-decrypt-recursive (ssm-path)
   (interactive "sSSM path? ")
   (shell-command (format "aws ssm get-parameters-by-path --path %s --recursive --with-decryption"
                          ssm-path)))
