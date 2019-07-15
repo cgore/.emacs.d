8f19ee6eb2c70e6a30785b6bc737b7940519175c
Christopher Mark Gore <cgore@cgore.com>
2019-07-15 16:25:36 -0500

    s3-ls

--- aws.el
+++ aws.el
@@ -1,5 +1,10 @@
+(defun s3-ls (s3-uri)
+  (interactive "sS3 URI (s3://some-bucket/some/prefix)? ")
+  (shell-command (format "aws s3 ls %s --recursive --human-readable --summarize"
+                         s3-uri)))
+
 (defun ssm-decrypt (ssm-parameter-name)
-  (interactive "sSSM path? ")
+  (interactive "sSSM parameter? ")
   (shell-command (format "aws ssm get-parameter --name %s --with-decryption"
                          ssm-parameter-name)))
 
