d110837888579731c887c01f4341cec2704ae84c
Christopher Mark Gore <cgore@cgore.com>
2019-10-31 13:42:44 -0500

    Disabling with-editor for multi-term, I am mostly using non-shells there

--- git.el
+++ git.el
@@ -7,7 +7,6 @@
 
 ;; https://github.com/magit/with-editor
 (add-hook 'shell-mode-hook  'with-editor-export-editor)
-(add-hook 'term-exec-hook   'with-editor-export-editor)
 (add-hook 'eshell-mode-hook 'with-editor-export-editor)
 
 (defun git-pull-rebase-master ()
