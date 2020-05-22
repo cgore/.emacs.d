7c85dbac5c2f4ce7a10dfe28430558ac6e57e92b
Christopher Mark Gore <chris.gore@rate.com>
2020-05-22 06:59:13 -0500

    Some JavaScript tweaks

--- javascript.el
+++ javascript.el
@@ -53,10 +53,15 @@
 ;;     M-, Pop back to where M-. was last invoked
 (require 'xref-js2)
 (define-key js2-mode-map (kbd "M-.") nil)
-(add-hook 'js2-mode-hook (lambda ()
-                           (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
-(add-hook 'rjsx-mode-hook (lambda ()
-                           (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+(define-key js2-mode-map
+  ;; This is normally on M-? but this is easier to type and I never use the
+  ;; abbreviation stuff anyway, so I'll make the unshifted version work too.
+  (kbd "M-/")
+  'xref-find-references)
+(let (hook (lambda ()
+             (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+  (add-hook 'js2-mode-hook  hook)
+  (add-hook 'rjsx-mode-hook hook))
 
 ;; https://github.com/felipeochoa/rjsx-mode
 (require 'rjsx-mode)
