12034b5c433bcd19381751717bd15a69f00f5a5a
Christopher Mark Gore <chris.gore@rate.com>
2020-05-30 22:09:43 -0500

    install some default things

--- install.el
+++ install.el
@@ -15,3 +15,38 @@
 
 (defun brew-cask-install (thing)
   (shell-command (concat "brew cask install " thing)))
+
+(when (darwin?)
+    (brew-install "git")
+  ;; echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.zshrc
+  ;; export LDFLAGS="-L/usr/local/opt/gettext/lib"
+  ;; export CPPFLAGS="-I/usr/local/opt/gettext/include"
+
+  (brew-tap "homebrew/cask-fonts")
+  (brew-cask-install "font-fira-code")
+
+  (brew-install "bash")
+
+  (brew-install "zsh")
+  ;; echo 'export PATH="/usr/local/opt/ncurses/bin:$PATH"' >> ~/.zshrc
+  ;; export LDFLAGS="-L/usr/local/opt/ncurses/lib"
+  ;; export CPPFLAGS="-I/usr/local/opt/ncurses/include"
+
+  (brew-install "awscli")
+
+  (brew-install "ack")
+
+
+  (brew-install "vault")
+  (brew-install "python-yq")
+  (brew-install "leiningen")
+
+  ;; Our for-real prod instances are mostly running OpenJDK, not Oracle's Java.
+  ;; sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+  ;; echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+  ;; export CPPFLAGS="-I/usr/local/opt/openjdk/include"
+  (brew-install "openjdk")
+
+  ;; https://github.com/Homebrew/homebrew-core/issues/50536
+  ;; https://clojure.org/news/2020/02/28/clojure-tap
+  (brew-install "clojure/tools/clojure"))
