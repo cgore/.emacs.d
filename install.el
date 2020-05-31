661a6db9a60534fbac730718800f78381e0c5ea1
Christopher Mark Gore <chris.gore@rate.com>
2020-05-30 22:10:48 -0500

    wrap in a fn

--- install.el
+++ install.el
@@ -16,36 +16,37 @@
 (defun brew-cask-install (thing)
   (shell-command (concat "brew cask install " thing)))
 
-(when (darwin?)
+(defun initial-setup ()
+  (when (darwin?)
     (brew-install "git")
-  ;; echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.zshrc
-  ;; export LDFLAGS="-L/usr/local/opt/gettext/lib"
-  ;; export CPPFLAGS="-I/usr/local/opt/gettext/include"
+    ;; echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.zshrc
+    ;; export LDFLAGS="-L/usr/local/opt/gettext/lib"
+    ;; export CPPFLAGS="-I/usr/local/opt/gettext/include"
 
-  (brew-tap "homebrew/cask-fonts")
-  (brew-cask-install "font-fira-code")
+    (brew-tap "homebrew/cask-fonts")
+    (brew-cask-install "font-fira-code")
 
-  (brew-install "bash")
+    (brew-install "bash")
 
-  (brew-install "zsh")
-  ;; echo 'export PATH="/usr/local/opt/ncurses/bin:$PATH"' >> ~/.zshrc
-  ;; export LDFLAGS="-L/usr/local/opt/ncurses/lib"
-  ;; export CPPFLAGS="-I/usr/local/opt/ncurses/include"
+    (brew-install "zsh")
+    ;; echo 'export PATH="/usr/local/opt/ncurses/bin:$PATH"' >> ~/.zshrc
+    ;; export LDFLAGS="-L/usr/local/opt/ncurses/lib"
+    ;; export CPPFLAGS="-I/usr/local/opt/ncurses/include"
 
-  (brew-install "awscli")
+    (brew-install "awscli")
 
-  (brew-install "ack")
+    (brew-install "ack")
 
 
-  (brew-install "vault")
-  (brew-install "python-yq")
-  (brew-install "leiningen")
+    (brew-install "vault")
+    (brew-install "python-yq")
+    (brew-install "leiningen")
 
-  ;; sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-  ;; echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
-  ;; export CPPFLAGS="-I/usr/local/opt/openjdk/include"
-  (brew-install "openjdk")
+    ;; sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+    ;; echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+    ;; export CPPFLAGS="-I/usr/local/opt/openjdk/include"
+    (brew-install "openjdk")
 
-  ;; https://github.com/Homebrew/homebrew-core/issues/50536
-  ;; https://clojure.org/news/2020/02/28/clojure-tap
-  (brew-install "clojure/tools/clojure"))
+    ;; https://github.com/Homebrew/homebrew-core/issues/50536
+    ;; https://clojure.org/news/2020/02/28/clojure-tap
+    (brew-install "clojure/tools/clojure")))
