418c291b4404a61106c9fdb11541fd78c74c6ec7
Christopher Mark Gore <cgore@cgore.com>
2021-03-12 23:34:51 -0600

    specifying sbcl

--- common-lisp.el
+++ common-lisp.el
@@ -1,15 +1,17 @@
+(setq inferior-lisp-program "sbcl")
+
 ;; (setq inferior-lisp-program "/usr/bin/env sbcl --noinform --no-linedit")
 
 ;; (load (expand-file-name "~/.roswell/helper.el"))
 
-(setq sly-lisp-implementations
-      (append (cl-remove-if-not (lambda (pair) (executable-find (caadr pair)))
-                                '((lispworks ("lw-console"))
-                                  (sbcl ("sbcl"))
-                                  (ccl-x64 ("ccl64"))
-                                  (ccl-arm ("armcl"))
-                                  (ecl ("ecl"))
-                                  (clisp ("clisp"))))
-              (let ((path (locate-file "abcl.jar" exec-path)))
-                (when path
-                  `((abcl ("java" "-jar" ,path)))))))
+;; (setq sly-lisp-implementations
+;;       (append (cl-remove-if-not (lambda (pair) (executable-find (caadr pair)))
+;;                                 '((lispworks ("lw-console"))
+;;                                   (sbcl ("sbcl"))
+;;                                   (ccl-x64 ("ccl64"))
+;;                                   (ccl-arm ("armcl"))
+;;                                   (ecl ("ecl"))
+;;                                   (clisp ("clisp"))))
+;;               (let ((path (locate-file "abcl.jar" exec-path)))
+;;                 (when path
+;;                   `((abcl ("java" "-jar" ,path)))))))
