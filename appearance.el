cdcb91b8fe9f1b851b91b5927a1d650c02f03c7f
Christopher Mark Gore <cgore@cgore.com>
2021-10-25 12:48:58 -0700

    Some tweaks for a more modern OSX Homebrew

--- appearance.el
+++ appearance.el
@@ -51,7 +51,7 @@
 OSX:
 
 brew tap homebrew/cask-fonts
-brew cask install font-fira-code
+brew install --cask font-fira-code
 
 Ubuntu:
 
--- init.el
+++ init.el
@@ -338,7 +338,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :family "Fira Code"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Fira Code"))))
  '(whitespace-tab ((t (:foreground "#636363")))))
 (put 'erase-buffer 'disabled nil)
 
