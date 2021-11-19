673b8dd500a414bc663d217d9548f47a258412af
Christopher Mark Gore <cgore@cgore.com>
2021-11-19 13:39:46 -0600

    Sticking revert-buffer somewhere convenient

--- git.el
+++ git.el
@@ -1,8 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.gitconfig" . conf-mode))
 (global-set-key (kbd "<f2> <f2>") 'magit-status)
-(global-set-key (kbd "<f2> g") 'magit-status)
-(global-set-key (kbd "<f2> l") 'magit-log-current)
-(global-set-key (kbd "<f2> t") 'magit-log-all)
+(global-set-key (kbd "<f2> g")    'magit-status)
+(global-set-key (kbd "<f2> l")    'magit-log-current)
+(global-set-key (kbd "<f2> t")    'magit-log-all)
+(global-set-key (kbd "<f2> !")    'revert-buffer)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 ;; https://github.com/magit/with-editor
