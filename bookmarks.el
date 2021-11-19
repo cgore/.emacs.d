28b845e7655b6e4f39847d3d47a25ce40f5572be
Christopher Mark Gore <cgore@cgore.com>
2021-11-19 14:21:30 -0600

    Nicer places for the bookmark fns

---
+++ bookmarks.el
@@ -0,0 +1,4 @@
+(global-set-key (kbd "<f7> +") 'bookmark-set)
+(global-set-key (kbd "<f7> -") 'bookmark-delete)
+(global-set-key (kbd "<f7> ^") 'bookmark-jump)
+(global-set-key (kbd "<f7> *") 'bookmark-bmenu-list)
