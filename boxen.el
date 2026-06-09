49ab5af8ecb6888f47fa9f05934ee6cd180fc577
Christopher Mark Gore <cgore@cgore.com>
2026-06-09 01:27:15 -0500

    Cleanup of the boxen code

--- boxen.el
+++ boxen.el
@@ -11,24 +11,31 @@
   "Iff this is windows-nt, basically raw Windows."
   (eq system-type 'windows-nt))
 
+(defun cgore-host? (short-name)
+  (or (string= (system-name) short-name)
+      (string= (system-name) (concat short-name ".cgore.com"))))
+
 (defun abaddon? ()
   "This was my old Camber workstation."
-  (string= (system-name) "abaddon"))
+  (cgore-host? "abaddon"))
+(defun alamoth? ()
+  "This is my Fedora VM on Habakkuk"
+  (cgore-host? "alamoth"))
 (defun corinth? ()
   "This is my personal Samsung RV510 laptop (2012)."
-  (string= (system-name) "corinth"))
+  (cgore-host? "corinth"))
 (defun ezekiel? ()
   "This is my Linode in California."
-  (string= (system-name) "ezekiel"))
+  (cgore-host? "ezekiel"))
 (defun habakkuk? ()
   "This is my very-long-lived yuge tower at  home."
-  (string= (system-name) "habakkuk"))
+  (cgore-host? "habakkuk"))
 (defun naaman? ()
   "This is my Linode in Atlanta."
-  (string= (system-name) "naaman"))
+  (cgore-host? "naaman"))
 (defun nephesh? ()
   "This is my personal MacBook Pro 15\" from 2014, formerly my Outpace laptop."
-  (string= (system-name) "nephesh"))
+  (cgore-host? "nephesh"))
 
 (require 's) ; https://github.com/magnars/s.el
 
